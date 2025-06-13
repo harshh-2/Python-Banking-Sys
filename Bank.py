55c2f9190af00e6af146e8b3ef6425d54c66dcd2
harshh-2 <harsh2dubey@gmail.com>
2025-06-13 14:50:16 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,3 +1,5 @@
+import random
+import time
 class Account:
     def __init__(self,bal,acc,name):
         self.bal=bal
@@ -13,56 +15,69 @@ class Account:
         print("Rs",amount,"debited")
         print("Remaining balance is:",self.bal)
     def bank(self):
-        a=input("DEBIT,CREDIT OR CHECK BALANCE:  ").upper()
-        if a=='DEBIT':
-            b=int(input("Amount to be debited:"))
-            if b>self.bal:
-                print("Amount exceeds current balance")
+        while True:
+            a=input("DEBIT,CREDIT OR CHECK BALANCE OR EXIT:  ").upper()
+            if a=='DEBIT':
+                b=int(input("Amount to be debited:"))
+                if b>self.bal:
+                    print("Amount exceeds current balance")
+                else:
+                    self.debit(b)
+            elif a=="CHECK BALANCE":
+                print(f"The current bank balance is : {self.bal}")
+            elif a=='CREDIT':
+                b=int(input("Amount to be credited:"))
+                if b<1:
+                    print("Amount to be credited should be greater than 1")
+                else:
+                    self.credit(b)
+            elif a == "EXIT":
+                print("Logging out from banking operations...\n")
+                break
             else:
-                self.debit(b)
-        elif a=="CHECK BALANCE":
-            print(f"The current bank balance is : {self.bal}")
-        elif a=='CREDIT':
-            b=int(input("Amount to be credited:"))
-            if b<1:
-                print("Amount to be credited should be greater than 1")
-            else:
-                self.credit(b)
+                print("Invalid operation")
+def menu():
+    print('                 Menu')
+    a=int(input("1 For Creating Account\n2 For Login to existing Account\n"))
+    return a
+
+user={}
+while True:
+    a=menu()
+    if a==1:
+        pn=int(input("Enter mobile number"))
+        if pn in user:
+            print("Account already exists")
         else:
-            print("Invalid operation")
-n=int(input("Enter Bank account number:"))
-s=0
-if n==1234:
-    r=input("Enter password:")
-    password='123er'
-    if r==password:
-        s=10000
-        name='Karan'
+            name=input("Enter your Name:")
+            pwd=input("Enter password")
+            n=0
+            s=random.randint(1000,9999)
+            user[s]=[name,pwd,n,pn]
+            print("Account created successfully!!")
+            print(f'Your Account Number is:{s}')
+            time.sleep(1)
+
+    elif a==2:
+        s=int(input("Enter Account Number"))
+        if s in user:
+            pwd=input("Enter password")
+            if pwd==user[s][1]:
+                s1 = Account(user[s][2], s, user[s][0])
+                time.sleep(1)
+                s1.bank()
+                user[s][2] = s1.bal 
+            else:
+                print("Incorect Paassword")
+        else:
+            print("User doesnt exist")
+        break
+    
     else:
-        print("Incorrect password exitinggg....")
-        exit()
-elif n==1245:
-  r=input("Enter password:")
-  password='123qwe'
-  if r==password:
-        s=75000
-        name='rahul'
-  else:
-        print("Incorrect password exitinggg....")
-        exit()
-  
-elif n==6969:
-    r=input("Enter password:")
-    password='chimpui'
-    if r==password:
-        name="Pranay"
-        s=12
-    else:
-        print("Incorrect password exitingg....")
-        exit()
-   
-else:
-    print("Account does not exist in this bank")
-    exit()
-s1=Account(s,n,name)
-s1.bank()
+        print("Invalid option")
+        break
+
+                
+
+
+
